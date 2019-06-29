5f46a243f7b0042380e88c4cc0b9a03775ed1025
tstoff <tim.stoffel@eventim.de>
2019-06-29 21:43:47 +0200

    added task6 for using an external jquery plugin

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # jQuery Workshop
 Starting small, we use this template to fiddle around with jQuery.
 
-## Fifth Task:
-- 
+## Sixth Task:
+- Use the Slick Plugin to build an Image Slider for the Band Images
+    - https://kenwheeler.github.io/slick/
+- Let the Slider show 2 Images and slide 1 Image at a time.
+
 Have fun :-)
--- obj/js/scripts.js
+++ obj/js/scripts.js
@@ -1,33 +1,3 @@
 jQuery(function(){
-    var artistImage = jQuery('.artistImage'), // <-- best practice to never use a selector twice!
-        artistText  = jQuery('.artistText');  //
-        serverResponse = {};
-
-    jQuery.ajax({
-        type: "POST",
-        url: "", // url should look like: 192.0.0.1:8080/artists
-    }).done(function(data){
-        serverResponse = JSON.parse(data);
-    }).fail(function(err){
-        console.error(err);
-    });
-
-    artistImage.fadeTo(1000, 0.5); //    <-- ANIMATED OPACITY
     
-    var grid = jQuery('content');
-
-    grid.on('mouseover', '.artistImage', function(){
-        jQuery(this).fadeTo(200, 1.0);
-    });
-
-    grid.on('mouseleave', '.artistImage', function(){
-        jQuery(this).fadeTo(200, 0.5);
-    });
-
-    grid.on('click', '.artistImage', function(){
-        var artistId = jQuery(this).attr('data-artist');
-        artistText.find('.artistHeadline').html(serverResponse.artists[artistId].name);
-        artistText.find('.artistDescription').html(serverResponse.artists[artistId].text);
-        artistText.show();
-    });
 });
